6a5e3219f25bf082bcde10b929affaa99bebd4f4
Blaqcoder005 <blaqcoder005@gmail.com>
2026-04-18 13:19:32 +0000

    Update AuthContext.jsx

--- Trends/src/context/AuthContext.jsx
+++ Trends/src/context/AuthContext.jsx
@@ -14,6 +14,7 @@ export function AuthProvider({ children }) {
         credentials: "include",
       });
       const data = await res.json();
+      console.log("checkAuth response:", data);
 
       if (data.authenticated) {
         setUser(data);
