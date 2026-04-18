a7204ba9a4bb5449a07e404b5a254dcb38c9db19
Blaqcoder005 <blaqcoder005@gmail.com>
2026-04-18 13:15:55 +0000

    Update ProtectedRoute.jsx

--- Trends/src/components/ProtectedRoute.jsx
+++ Trends/src/components/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import { useAuth } from "../context/AuthContext";
 
 export default function ProtectedRoute() {
   const { user, loading } = useAuth();
-  if (loading) return null;
+  if (loading) return <div>Loading...</div>;
   if (!user) return <Navigate to="/login" />;
-  return <Outlet />;  // ✅ renders nested routes
+  return <Outlet />;
 }
