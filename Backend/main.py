7e71e4abfca9b652e6934899ea8686b44372be89
Blaqcoder005 <blaqcoder005@gmail.com>
2026-04-18 10:12:47 +0000

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://blog-website-2twh.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
